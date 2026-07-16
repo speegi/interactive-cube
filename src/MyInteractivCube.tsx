bb62273833bb2cf7c6862359834470dd3fed8c77
speegi <lolspeeg@gmail.com>
2026-07-16 18:49:40 +0200

    Add @react-spring/three for animations and refactor MyInteractivCube component

--- src/MyInteractivCube.tsx
+++ src/MyInteractivCube.tsx
@@ -1,4 +1,5 @@
-import { MeshDistortMaterial, Resize } from "@react-three/drei";
+import { animated, useSpring } from "@react-spring/three";
+import { MeshDistortMaterial } from "@react-three/drei";
 import { useState } from "react";
 
 export default function MyInteractivCube()
@@ -6,19 +7,26 @@ export default function MyInteractivCube()
 
     const[hover,setHover] = useState(false)
     const[toggle,setToggle]= useState(false)
+    const { scale } = useSpring({
+        scale: toggle ? 1 : 1.4,
+        config: { mass: 1, tension: 200, friction: 20 }
+    });
+
     return(
-        <Resize scale={toggle?1:1.4}>
-            <mesh  receiveShadow castShadow
-
-                    onPointerOver={(event)=>setHover(true)}
-                    onPointerOut={(event)=>setHover(false)}
-                    onClick={(event)=>setToggle(!toggle)}
-                >
-                  <boxGeometry args={[1,1,1,15,15,15]}/>
-
-                  <MeshDistortMaterial color={hover?'blue':'gray'} distort={1} speed={1}/>        
-            </mesh>
-        </Resize>
+        
+            <animated.mesh  
+                receiveShadow 
+                castShadow
+                scale={scale}
+                
+                onPointerOver={(event)=>setHover(true)}
+                onPointerOut={(event)=>setHover(false)}
+                onClick={(event)=>setToggle(!toggle)}
+            >
+                <boxGeometry args={[1,1,1,15,15,15]}/>
+                <MeshDistortMaterial color={hover?'blue':'gray'} distort={1} speed={1}/>        
+            </animated.mesh>
+       
     )
 
 }
